ba6e7d59c9b97d53352c6b2e7033d08e92155ae8
Yusuf Safrudin <y.safrudin666@gmail.com>
2018-03-26 13:46:35 +0700

    Menambahkan Comment

--- app/src/main/java/com/mythcon/savr/ngelih/Cart.java
+++ app/src/main/java/com/mythcon/savr/ngelih/Cart.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,7 @@ import com.mythcon.savr.ngelih.Databases.Database;
 import com.mythcon.savr.ngelih.Model.Order;
 import com.mythcon.savr.ngelih.Model.Request;
 import com.mythcon.savr.ngelih.ViewHolder.CartAdapter;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -85,13 +87,13 @@ public class Cart extends AppCompatActivity {
         alertDialog.setTitle("One more step");
         alertDialog.setMessage("Enter yout address");
 
-        final EditText edtAddress = new EditText(Cart.this);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-        );
-        edtAddress.setLayoutParams(layoutParams);
-        alertDialog.setView(edtAddress);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View order_address_comment = inflater.inflate(R.layout.order_address_comment,null);
+
+        final MaterialEditText edtAddress = order_address_comment.findViewById(R.id.edtAddress);
+        final MaterialEditText edtComment = order_address_comment.findViewById(R.id.edtComment);
+
+        alertDialog.setView(order_address_comment);
         alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
 
         alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
@@ -103,6 +105,8 @@ public class Cart extends AppCompatActivity {
                         Common.currentUser.getName(),
                         edtAddress.getText().toString(),
                         txtTotalPrice.getText().toString(),
+                        "0",
+                        edtComment.getText().toString(),
                         cart
                 );
 
